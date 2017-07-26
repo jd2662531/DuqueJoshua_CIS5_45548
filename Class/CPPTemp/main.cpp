359539c0be6bec6db60797df0dcc05b4a47a8e73
Joshua Duque <jordanscraper8@gmail.com>
2017-07-26 14:17:05 -0700

    Signed-off-by: Joshua Duque <jordanscraper8@gmail.com>

---
+++ Class/CPPTemp/main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.cpp
+ * Author: Joshua Duque
+ * Created on 
+ * Purpose:  
+ */
+
+//System Libraries
+#include <iostream>  //Input - Output Library
+using namespace std; //Name-space under which system libraries exist
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare variables
+    
+    //Initialize variables
+    
+    //Input data
+    
+    //Map inputs to outputs or process the data
+    
+    //Output the transformed data
+    
+    //Exit stage right!
+    return 0;
+}
